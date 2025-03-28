eeb639774c97c1276cc6dab4ea1420859f940566
faiq <faiq@voltras.co.id>
2025-03-28 07:27:48 +0700

    0.0.3

--- lib/src/models/user_profile/user_profile_request.dart
+++ lib/src/models/user_profile/user_profile_request.dart
@@ -1,21 +1,7 @@
 import 'package:equatable/equatable.dart';
 
-class CreateUserProfileRequest extends Equatable {
-  const CreateUserProfileRequest({required this.firstName, required this.lastName});
-
-  final String firstName;
-  final String lastName;
-
-  Map<String, dynamic> toJson() {
-    return {'first_name': firstName, 'last_name': lastName};
-  }
-
-  @override
-  List<Object?> get props => [firstName, lastName];
-}
-
-class UpdateUserProfileRequest extends Equatable {
-  const UpdateUserProfileRequest({this.firstName, this.lastName});
+class UserProfileRequest extends Equatable {
+  const UserProfileRequest({this.firstName, this.lastName});
 
   final String? firstName;
   final String? lastName;
